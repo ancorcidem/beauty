cc96b8eee3c0efb66aef347e89e3ff2b1c1405d3
ancorcidem <ancorcidem@gmail.com>
2013-02-21 17:32:50 +0300

    Initial commit

---
+++ Beauty.Specs.UI.Bindings/BeautyStepDefinition.cs
@@ -0,0 +1,26 @@
+﻿using TechTalk.SpecFlow;
+
+namespace Beauty.Specs.UI.StepDefinitions
+{
+    [Binding]
+    public class BeautyStepDefinition
+    {
+        [Given(@"beauties aging (.*), (.*), (.*), (.*)")]
+        public void GivenBeautiesAging(int p0, int p1, int p2, int p3)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"search for a beauty between (.*) and (.*) years old")]
+        public void WhenSearchForABeautyBetweenAndYearsOld(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"found girls should be (.*), (.*)")]
+        public void ThenFoundGirlsShouldBe(int p0, int p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
